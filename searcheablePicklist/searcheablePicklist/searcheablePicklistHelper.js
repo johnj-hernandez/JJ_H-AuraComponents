f3369b299c60d83894103188920c1e2275bbd0a0
johnj-hernandez <46012042+johnj-hernandez@users.noreply.github.com>
2020-08-04 12:27:51 -0500

    Update searcheablePicklistHelper.js

--- searcheablePicklist/searcheablePicklist/searcheablePicklistHelper.js
+++ searcheablePicklist/searcheablePicklist/searcheablePicklistHelper.js
@@ -1,45 +1,5 @@
 ({
-    searchHelper1: function (component, event, getInputkeyWord) {
-        // call the apex class method 
-        var action = component.get("c.fetchLookUpValues");
-        // set param to method  
-        action.setParams({
-            'searchKeyWord': getInputkeyWord,
-            'ObjectName': component.get("v.objectAPIName")
-        });
-        // set a callBack    
-        action.setCallback(this, function (response) {
-            $A.util.removeClass(component.find("mySpinner"), "slds-show");
-            var state = response.getState();
-            if (state === "SUCCESS") {
-                var storeResponse = response.getReturnValue();
-                // if storeResponse size is equal 0 ,display No Result Found... message on screen.                }
-                if (storeResponse.length == 0) {
-                    component.set("v.Message", 'No Result Found...');
-                } else {
-                    component.set("v.Message", '');
-                }
-                // set searchResult list with return value from server.
-                component.set("v.listOfSearchRecords", storeResponse);
-            }
-
-        });
-        // enqueue the Action  
-        $A.enqueueAction(action);
-
-    },
     searchHelper: function (component, event, getInputkeyWord) {
-        var dummyArray = [];
-        for (let i = 0; i < 20; i++) {
-            const element = {
-                label: "label" + i,
-                value: "value" + i
-            };
-            dummyArray.push(element);
-
-        }
-        console.log('dummyArray');
-        console.log(dummyArray);
         //component.set("v.fullList", dummyArray);
         var fullList = component.get("v.fullList");
         var storeResponse = [];
